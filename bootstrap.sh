d0e5ec9b49afdb5d76c1a82d558d5e0a92a1e030
Swaraj Bhanja <swarajbhanja607@gmail.com>
2026-04-15 06:28:41 +0700

    Standardize

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,13 @@ yum update -y
 yum install -y git
 yum install -y docker
 mkdir -p /usr/local/lib/docker/cli-plugins
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+curl -fL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 \
   -o /usr/local/lib/docker/cli-plugins/docker-compose
 chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-curl -SL https://github.com/docker/buildx/releases/latest/download/buildx-v0.17.1.linux-amd64 \
+curl -fL https://github.com/docker/buildx/releases/download/v0.17.1/buildx-v0.17.1.linux-amd64 \
   -o /usr/local/lib/docker/cli-plugins/docker-buildx
 chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+systemctl restart docker
